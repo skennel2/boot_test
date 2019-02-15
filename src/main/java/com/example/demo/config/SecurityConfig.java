cb2d99a655d52827a9c41986b5e3a68f6fb7f3f6
skennel2 <skennel2@gmail.com>
2019-02-15 18:15:51 +0900

    소스정리

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			public String encode(CharSequence rawPassword) {
 				return rawPassword.toString();
 			}
-		});
+		});	
 	}
 	
 	@Override
